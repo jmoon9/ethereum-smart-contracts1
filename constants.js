c18af3215f6de0658bfa96d4391f3ef8e912b311
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2020-06-21 16:20:53 -0500

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,7 @@
+MNEMONIC = 'quiz common ask feature trade scan crew similar season foot timber direct';
+NETWORK_URL = 'https://rinkeby.infura.io/v3/0881ce0fe1d34cd2bcf719368000c434';
+
+module.exports = {
+    MNEMONIC,
+    NETWORK_URL
+};
